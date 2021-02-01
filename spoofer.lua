f03fcf0112f58871488e9f56950d61552cdc6b82
poketik12311 <70782758+poketik12311@users.noreply.github.com>
2021-02-01 13:42:16 +0300

    Update spoofer.lua

--- spoofer.lua
+++ spoofer.lua
@@ -1,5 +1,32 @@
-entity.get_steam64 = function(id)
-    if (id == entity.get_local_player()) then
-        return 1031513426
-end
-end
+local inverter = ui.new_hotkey("AA", "Anti-aimbot angles", "Inverter (Legit AA)")
+
+client.set_event_callback("setup_command", function(info)
+    if info.in_use == 1 then 
+        if info.in_grenade1 == 1 or info.in_grenade2 == 1 or info.in_attack == 1 then return end
+        local cam_anglesx, cam_anglesy, cam_anglesr = client.camera_angles()
+        local tick_count = globals.tickcount() % 3
+        if ui.get(inverter) then
+            if tick_count == 1 then
+                info.yaw = cam_anglesy + 120
+                info.allow_send_packet = false
+            elseif tick_count == 2 then
+                info.yaw = cam_anglesy - 120
+                info.allow_send_packet = false
+            else
+                info.yaw = cam_anglesy
+                info.allow_send_packet = true
+            end
+        else
+            if tick_count == 1 then
+                info.yaw = cam_anglesy - 120
+                info.allow_send_packet = false
+            elseif tick_count == 2 then
+                info.yaw = cam_anglesy + 120
+                info.allow_send_packet = false
+            else
+                info.yaw = cam_anglesy
+                info.allow_send_packet = true
+            end
+        end
+    end
+end)
