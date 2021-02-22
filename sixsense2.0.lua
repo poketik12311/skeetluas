14fb67dbf10286080decfdda91a221ba10c3f6df
poketik12311 <70782758+poketik12311@users.noreply.github.com>
2021-02-22 20:22:04 +0300

    231321231123123
    213231

--- sixsense2.0.lua
+++ sixsense2.0.lua
@@ -1,35 +1,40 @@
-local ui_get, exec, uid_eindex, local_player = ui.get, client.exec, client.userid_to_entindex, entity.get_local_player
-
-local hitsounds = {
-    "-",
-    "Default",
-    "LUA error",
-    "Warning",
-    "Custom"
+local script = {
+    check = ui.new_checkbox("visuals", "player esp", "Hit marker sound"),
+    chs = ui.new_textbox("visuals", "player esp", "custom hitsound"),
+    hs = ui.reference("visuals", "player esp", "hit marker sound"),
+    vol = ui.new_slider("visuals", "player esp", "\n", 1, 10, 10, true, "", 0.1),
+    check_def = ui.new_checkbox("visuals", "player esp", "Use default sound")
 }
+ui.set_visible(script.hs, false)
 
-local hitsound  = ui.new_combobox("Visuals", "Player ESP", "Hit sound", hitsounds)
+local function menu()
+    ui.set_visible(script.chs, ui.get(script.check))
+    ui.set_visible(script.check_def, ui.get(script.check))
+    ui.set_visible(script.vol, ui.get(script.check))
+end
+
+ui.set_callback(script.check, menu)
+menu()
 
 client.set_event_callback("player_hurt", function(e)
-    local hitsound_get = ui_get(hitsound)
-
-    if hitsound_get == "-" then return end
-    
-    local attacker = e.attacker
-    
-    if attacker == nil then return end
-
-    local attacker_entindex = uid_eindex(attacker)
-
-    if attacker_entindex == local_player() then
-        if hitsound_get == "Default" then
-            exec("playvol buttons\\arena_switch_press_02.wav 1")
-        elseif hitsound_get == "LUA error" then
-            exec("playvol ui\\weapon_cant_buy.wav 1")
-        elseif hitsound_get == "Warning" then
-            exec("playvol resource\\warning.wav 1")
-        elseif hitsound_get == "Custom" then
-            exec("playvol *\\bow_ding.wav 1")
+    local attacker = client.userid_to_entindex(e.attacker)
+    local lp = entity.get_local_player()
+    local volume = ui.get(script.vol)/10
+	
+    if ui.get(script.check) then
+        if ui.get(script.chs) ~= "" then
+		    if attacker == lp then
+                client.exec("playvol */", ui.get(script.chs), " ", volume)
+                ui.set(script.hs, ui.get(script.check_def))
+            end
+        else
+            ui.set(script.hs, true)
         end
-    end
+    else
+        ui.set(script.hs, false)
+	end
+end)
+
+client.set_event_callback("shutdown", function()
+    ui.set_visible(script.hs, true)
 end)
